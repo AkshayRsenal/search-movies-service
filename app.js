69b0ec695d55bc3657e0f753179e360411858c18
Akshay-Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-10 22:17:24 +0200

    CORS resolve

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const request = require('request');
 const elasticConfig = config.get('elastic');
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 const client = new Client({
     cloud: {
@@ -15,6 +15,14 @@ const client = new Client({
         password: elasticConfig.password
     }
 })
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 app.get('/', (req, res) => {
     res.send('finished landing')
