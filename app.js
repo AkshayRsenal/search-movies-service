921cc4b0fe11d9a1c1b35f08c762e19821b8dcaf
Akshay-Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-13 14:04:52 +0200

    adding service routes

---
+++ app.js
@@ -0,0 +1,76 @@
+const { Client } = require('@elastic/elasticsearch')
+const config = require('config');
+const request = require('request');
+const elasticConfig = config.get('elastic');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+const client = new Client({
+    cloud: {
+        id: elasticConfig.cloudID
+    },
+    auth: {
+        username: elasticConfig.username,
+        password: elasticConfig.password
+    }
+})
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+app.get('/', (req, res) => {
+    res.send('finished landing')
+})
+
+app.get('/movies/:category/:keywords', (req, res) => {
+    let results = read(req.params.category, req.params.keywords);
+    results.then(X => res.send(JSON.stringify(X)))
+})
+
+app.get('/refresh', (req, res) => {
+    res.send('refresh completed')
+})
+
+
+
+async function read(category, keywords) {
+    if (category === 'all') {
+        var elasticSearchObject = {
+            query_string: {
+                query: keywords
+
+            }
+        }
+    } else {
+        var Initialobject = {}
+        Initialobject[category] = keywords
+        var elasticSearchObject = {
+            match: Initialobject
+        }
+        console.log(elasticSearchObject);
+    }
+
+    const { body } = await client.search({
+        index: 'movie_database',
+        size: 200,
+        body: {
+            query: elasticSearchObject/*  {
+                match: { category: keywords }
+            } */
+        }
+    })
+    return body.hits.hits
+}
+// read().catch(console.log)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at ${port}`)
+})
+
