35f47bf20012dbc1f5e7d47bf46ecd82b96539ed
Akshay-Raul <akshay.raul@stud.hochschule-heidelberg.de>
2021-05-26 11:56:01 +0200

    config

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const port = process.env.PORT || 5000
 
 const client = new Client({
     cloud: {
-        id: elasticConfig.cloudID
+        id: process.env.elasticCloudId
     },
     auth: {
-        username: elasticConfig.username,
-        password: elasticConfig.password
+        username: process.env.elasticUser,
+        password: process.env.elasticPassword
     }
 })
 
